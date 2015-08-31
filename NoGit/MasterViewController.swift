86d14bdd9a44756dc30b88574e700b63b92b6a8d
testeGit <matheus8471@gmail.com>
2015-08-31 16:09:02 -0300

    Deletei DENOVO

--- NoGit/MasterViewController.swift
+++ NoGit/MasterViewController.swift
@@ -14,13 +14,6 @@ class MasterViewController: UITableViewController {
     var objects = [AnyObject]()
 
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            self.clearsSelectionOnViewWillAppear = false
-            self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
