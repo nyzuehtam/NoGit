d5f03f3eb6f3ee4f3a69d1339069b67f6e0a3e22
testeGit <matheus8471@gmail.com>
2015-08-31 16:08:26 -0300

    Revert "Delete uma pa de coisa"
    This reverts commit e57dad02283b59cffe327fc700cf9fc47b5d0a38.

--- NoGit/MasterViewController.swift
+++ NoGit/MasterViewController.swift
@@ -14,6 +14,13 @@ class MasterViewController: UITableViewController {
     var objects = [AnyObject]()
 
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            self.clearsSelectionOnViewWillAppear = false
+            self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
